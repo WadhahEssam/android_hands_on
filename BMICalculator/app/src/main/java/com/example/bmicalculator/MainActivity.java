3882cf5dd859199d7c8156dca6ce1da751d64f2a
Wadah Mac Kraken <wadah-wleed@hotmail.com>
2020-12-04 10:23:13 +0300

    .

--- BMICalculator/app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ BMICalculator/app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast hello = Toast.makeText(this, "Welcome to the bugged one", Toast.LENGTH_LONG);
+        hello.show();
     }
 }
