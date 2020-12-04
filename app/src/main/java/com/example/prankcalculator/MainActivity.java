d93b8f7eab0a19a517881432e47ed490e6eafe16
Wadah Mac Kraken <wadah-wleed@hotmail.com>
2020-12-05 00:19:17 +0300

    half assed calculator

--- app/src/main/java/com/example/prankcalculator/MainActivity.java
+++ app/src/main/java/com/example/prankcalculator/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.prankcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+
         setContentView(R.layout.activity_main);
+
     }
 }
